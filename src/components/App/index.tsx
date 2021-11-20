394f5b459390d0cd799601f6cbafc0dc32db32ab
Taeil Kwak <Taeil2@gmail.com>
2021-11-19 22:32:32 -0800

    updated files and folder structure

---
+++ src/components/App/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+      </header>
+      <main>
+        <h1>Hi</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
