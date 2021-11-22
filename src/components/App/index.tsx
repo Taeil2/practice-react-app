6c933a7aea04b2fb02ce3c41455b62a77e2d14e3
Taeil Kwak <Taeil2@gmail.com>
2021-11-22 07:08:23 -0800

    fixed some package errors

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,19 @@
-import './styles.css';
+import './styles.css'
 
-function App() {
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button``
+
+function App(): JSX.Element {
   return (
     <div>
-      <header>
-      </header>
+      <header></header>
       <main>
         <h1>Hi</h1>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
